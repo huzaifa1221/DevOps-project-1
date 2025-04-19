181aea7b46b32085fc9785e9460277b037d843d0
Huzaifa <huzaifa.mushfiq101@gmail.com>
2025-04-19 14:33:49 +0530

    lb updated

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 module.exports = {
-    CORS_ORIGIN: 'http://lightfeather-alb-1930568892.us-east-1.elb.amazonaws.com:3000'
+    CORS_ORIGIN: 'http://lightfeather-alb-316290840.us-east-1.elb.amazonaws.com:3000'
 }
--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
 // export const API_URL = 'http://localhost:8080/'
-export const API_URL = 'http://lightfeather-alb-1930568892.us-east-1.elb.amazonaws.com:8080'
+export const API_URL = 'http://lightfeather-alb-316290840.us-east-1.elb.amazonaws.com:8080'
 export default API_URL
